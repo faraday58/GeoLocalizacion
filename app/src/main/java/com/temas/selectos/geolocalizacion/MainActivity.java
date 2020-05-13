804b69ea291e79e143d99eef406460666d4061f4
faraday58 <jarodriguezvera@gmail.com>
2020-05-13 14:23:00 -0500

    Solamente muestra latitud y longitud así como los proveedores de servicio  de localización.

---
+++ app/src/main/java/com/temas/selectos/geolocalizacion/MainActivity.java
@@ -0,0 +1,106 @@
+package com.temas.selectos.geolocalizacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_LOCATION = 100;
+    private LocationManager ubicacion;
+    private EditText edtLatitud;
+    private EditText edtLongitud;
+    private Button btnLocalizar;
+    private Spinner spnProveedores;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtLatitud=findViewById(R.id.edtLatitud);
+        edtLongitud=findViewById(R.id.edtLongitud);
+        btnLocalizar= findViewById(R.id.btnLocalizar);
+        spnProveedores= findViewById(R.id.spnProveedores);
+        btnLocalizar.setOnClickListener(onClickLocalizar);
+        validaGPS();
+        listaProveedores();
+
+
+
+    }
+
+    private void validaGPS() {
+
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ubicacion == null) {
+            Toast.makeText(this, "Su equipo no cuenta con ninguún proveedor de Localización", Toast.LENGTH_SHORT).show();
+        } else {
+             Toast.makeText(this,"Se requiere que el GPS se encuentre encendido",Toast.LENGTH_SHORT).show();
+            if (!ubicacion.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Intent intentGPS= new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intentGPS);
+
+
+            }
+        }
+
+
+
+    }
+
+
+    View.OnClickListener onClickLocalizar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            localizacion();
+        }
+    };
+
+
+    private void localizacion() {
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+            },REQUEST_CODE_LOCATION);
+        }
+        Location location = ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location!= null){
+            edtLongitud.setText(String.valueOf(location.getLongitude()));
+            edtLatitud.setText(String.valueOf(location.getLatitude()));
+        }
+    }
+
+    private void listaProveedores()
+    {
+        ubicacion= (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        List<String>listaProveedores= ubicacion.getAllProviders();
+        String [] proveedores= new String[listaProveedores.size()];
+        for(int i=0; i< proveedores.length; i++ )
+        {
+            LocationProvider proveedor = ubicacion.getProvider(listaProveedores.get(i));
+            proveedores[i]=proveedor.getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,proveedores);
+        spnProveedores.setAdapter(adapter);
+
+
+    }
+}
